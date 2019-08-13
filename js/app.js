5b0d35f4ba2bbde10c428982e227403f3c37b465
Pavitra Brahmbhatt <pavitrabrahmbhatt@Pavitras-MacBook-Air.local>
2019-08-13 15:04:28 -0500

    Links and images added

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// window.onload = function() {
+//   var c = document.getElementById("demo-canvas");
+//   var ctx = c.getContext("2d");
+//   var img = $("github")[0];
+//   ctx.drawImage(img, 10, 10, 150, 180);
+// };
+
+
+// const image = new Image();  // see note on creating an image
+// image.src = ('./images/github-logo.png');
+// image.onload = function(){
+//     //ctx.drawImage(this,0,0);
+// }
